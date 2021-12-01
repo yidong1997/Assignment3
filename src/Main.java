f7b016017aecdd1ff1855568246974b5f5f3e6a4
yidong1997 <wj196868@gmail.com>
2021-12-01 10:44:10 -0500

    "Added programmer comments, and starter code to method main, added my name to program output"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 
 public class Main {
 	public static void main(String[] args) {
-		
+		int roll = 0;
+		int[] diceRolls = new int[10];
+		int totalRolls = 0;
+		// sample the rolls
+		for(int count = 0; count < 1000; count++) {
+		    roll = (int)(Math.random() * 10) + 1;
+		    diceRolls[roll - 1] = diceRolls[roll - 1] + 1; // adjust 1-10 value to 0-9 for index
+		}
+		// run a report
+		for(int index = 0; index <= diceRolls.length; index++) {  // crashes
+		    System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+		    totalRolls = totalRolls + diceRolls[index];
+		}
+		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("Program by YidongWu");
 		
 	}
 }
